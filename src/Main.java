aeeaa64887f18c24dfc0f433269fea3abd322774
Olga Brazhkina <emelyanova84@yandex.ru>
2022-12-03 01:01:54 +0500

    Reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate (1.62, 54.5);
+        double bmi = service.calculate (54.5, 1.62);
         System.out.println("Ваш индекс массы тела - " + bmi);
     }
 }
